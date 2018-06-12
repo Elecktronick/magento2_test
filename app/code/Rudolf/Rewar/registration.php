a947afa483fe8ce5aec87f71882633fd6ba34906
Rudolf <Rudolf_gerber@mail.ru>
2018-06-12 15:08:27 +0300

    create new project

---
+++ app/code/Rudolf/Rewar/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Rudolf Gerber
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'rudolf_Rewar',
+    __DIR__
+);
